1d1e1dbfc3cbb2386758ee912bbb4ab16e8dd3d5
Sergey Onufrienko <sergey@onufrienko.com>
2020-04-10 10:44:51 +0100

    Memory optimization

--- handwritten-digits/index.js
+++ handwritten-digits/index.js
@@ -4,30 +4,40 @@ const LogisticRegression = require('./logistic-regression');
 const _ = require('lodash');
 const mnist = require('mnist-data');
 
-const trainingData = mnist.training(0, 1000);
-const testingData = mnist.testing(0, 100);
+const loadTrainingData = () => {
+  const trainingData = mnist.training(0, 20000);
+  const features = trainingData.images.values.map((image) => _.flatMap(image));
+  const encodedLabels = trainingData.labels.values.map((label) => {
+    const row = new Array(10).fill(0);
+    row[label] = 1;
+    return row;
+  });
 
-const features = trainingData.images.values.map((image) => _.flatMap(image));
-const encodedLabels = trainingData.labels.values.map((label) => {
-  const row = new Array(10).fill(0);
-  row[label] = 1;
-  return row;
-});
+  return { features, labels: encodedLabels }
+}
 
-const regression = new LogisticRegression(features, encodedLabels, {
+const loadTestingData = () => {
+  const testingData = mnist.testing(0, 1000);
+  const testFeatures = testingData.images.values.map((image) => _.flatMap(image));
+  const testEncodedLabels = testingData.labels.values.map((label) => {
+    const row = new Array(10).fill(0);
+    row[label] = 1;
+    return row;
+  });
+
+  return { testFeatures, testLabels: testEncodedLabels }
+}
+
+const { features, labels } = loadTrainingData();
+const regression = new LogisticRegression(features, labels, {
   learningRate: 1,
   iterations: 20,
   batchSize: 100,
 });
 
+debugger;
 regression.train();
 
-const testFeatures = testingData.images.values.map((image) => _.flatMap(image));
-const testEncodedLabels = testingData.labels.values.map((label) => {
-  const row = new Array(10).fill(0);
-  row[label] = 1;
-  return row;
-});
-
-const accuracy = regression.test(testFeatures, testEncodedLabels);
+const { testFeatures, testLabels } = loadTestingData();
+const accuracy = regression.test(testFeatures, testLabels);
 console.log(`Accuracy = ${accuracy}`);
